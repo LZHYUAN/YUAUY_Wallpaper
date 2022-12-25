4da004ad4436a75314a81a57698a55e97379f24d
BuildTools <unconfigured@null.spigotmc.org>
2022-12-26 00:43:46 +0800

    測試

--- YUAUY_Wallpaper/Program.cs
+++ YUAUY_Wallpaper/Program.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
 using System.Text;
 using System.Windows.Forms;
 
@@ -7,22 +9,54 @@ namespace YUAUY_Wallpaper
 {
     internal static class Program
     {
+        static string ImagePath = @"D:\YUAN\Pictures\орем";
         static void Main()
         {
             IntPtr WorkerWHwnd = GetWorkerWHwnd();
-            BufferedGraphics buffer = CreateBufferedGraphicsFromHwnd(WorkerWHwnd,true);
+            BufferedGraphics buffer = CreateBufferedGraphicsFromHwnd(WorkerWHwnd, true);
             RectangleF rectangle = buffer.Graphics.VisibleClipBounds;
-            var brush = new SolidBrush(Color.FromArgb(0, 0, 0, 0));
 
-            for (int i = 0; i < 255; i++)
+            buffer.Graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+
+            var images = new Queue<string>(Directory.GetFiles(ImagePath).Where(_ =>
+            _.ToUpper().EndsWith(".JPEG") ||
+            _.ToUpper().EndsWith(".JPG") ||
+            _.ToUpper().EndsWith(".PNG")));
+
+            rectangle.X = (Screen.AllScreens.Min(_ => _.Bounds.X));
+            rectangle.Y = (Screen.AllScreens.Min(_ => _.Bounds.Y));
+            Random random = new Random((int)DateTime.Now.Ticks);
+
+            while (true)
             {
-                brush.Color = Color.FromArgb(i, 0, 0, 0);
-                buffer.Graphics.FillRectangle(brush, rectangle);
+                var img = new Bitmap(images.Peek());
+                var max = Math.Max(img.Width, img.Height);
+                var rs = (double)random.Next(500, 1000) / max;
+                if (rs > 1) rs = 1;
+                var bmp = new Bitmap((int)(img.Width * rs) + 20, (int)(img.Height * rs) + 20, PixelFormat.Format32bppArgb);
+
+                var g = Graphics.FromImage(bmp);
+                g.Clear(Color.White);
+                g.DrawImage(img, 10, 10, (int)(img.Width * rs), (int)(img.Height * rs));
+
+                int s = random.Next(Screen.AllScreens.Length);
+                int w = random.Next(Screen.AllScreens[s].Bounds.Width);
+                int h = random.Next(Screen.AllScreens[s].Bounds.Height);
+                int x = Screen.AllScreens[s].Bounds.X + w;
+                int y = Screen.AllScreens[s].Bounds.Y + h;
+
+                buffer.Graphics.FillRectangle(new SolidBrush(Color.FromArgb(5, 0, 0, 0)), 0, 0, rectangle.Width, rectangle.Height);
+                var r = random.Next(-20, 20);
+                buffer.Graphics.RotateTransform(r);
+                buffer.Graphics.DrawImage(bmp, x - rectangle.X - bmp.Width / 2, y - rectangle.Y - bmp.Height / 2);
+                buffer.Graphics.RotateTransform(-r);
                 buffer.Render();
+
+                images.Enqueue(images.Dequeue());
+
+                Task.Delay(50).Wait();
             }
 
-            //ApplicationConfiguration.Initialize();
-            //Application.Run(new Form1());
         }
         static IntPtr GetWorkerWHwnd()
         {
