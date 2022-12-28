a5e00c56b3687ca9034bfe9fc1ad642a260d0121
BuildTools <unconfigured@null.spigotmc.org>
2022-12-29 04:17:39 +0800

    更新檔案檢查

--- YUAUY_Wallpaper/Image/ImagePicker.cs
+++ YUAUY_Wallpaper/Image/ImagePicker.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Drawing.Printing;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -15,67 +17,91 @@ namespace YUAUY_Wallpaper.Image
             Sort = ImagePickerSort.Name;
         }
         public List<string> ImageFolderList { get; private set; }
-        public ImagePickerSort Sort { get; set; }
+        public ImagePickerSort Sort
+        {
+            get
+            {
+                return _sort;
+            }
+            set
+            {
+                _sort = value;
+                _Refresh_imagePaths();
+            }
+        }
+        private ImagePickerSort _sort;
+        public int Count { get => _imagePaths.Count(); }
+        public readonly string[] SupportFiles =
+            {
+                "JPEG",
+                "JPG",
+                "PNG"
+            };
+
         public void Reset()
         {
             _index = -1;
         }
 
         private int _index = -1;
-        private IEnumerable<string> _imagePaths;
         public Bitmap? Next()
         {
             _index++;
-            if (_index > _imagePaths.Count()) _index = 0;
+            if (_index > _imagePaths.Length) _index = 0;
 
-            if (_imagePaths.Count() != ImageFolderList.Sum(path => Directory.GetFiles(path).Length) || // Tmp Paths Count != Now Paths Count
-              !File.Exists(_imagePaths.ElementAt(_index))) // or file not exists
+            _CheckAllFolder();
+            if (_lastFileCount != ImageFolderList.Sum(path => Directory.GetFiles(path).Length) || // tmp file count != now file count, For detection file add and remove
+              (_imagePaths.Length > 0 && !File.Exists(_imagePaths[_index]))) // or file not exists, For detection file change
             {
                 _Refresh_imagePaths();
-                if (_index > _imagePaths.Count()) _index = 0;
+                if (_index > _imagePaths.Length) _index = 0;
             }
 
-            if (_imagePaths.Count() > 0)
+            if (_imagePaths.Length > 0)
                 return new Bitmap(File.OpenRead(_imagePaths.ElementAt(_index)));
             else
                 return null;
         }
 
         private int _randomSeed = (int)DateTime.Now.Ticks;
+        private string[] _imagePaths;
+        private int _lastFileCount = 0;
         private void _Refresh_imagePaths()
         {
-            _CheckAllFolder();
-            _imagePaths = ImageFolderList.SelectMany(folderPath => Directory.GetFiles(folderPath));
+            var imagePathsTmp = ImageFolderList.SelectMany(folderPath => Directory.GetFiles(folderPath));
+            _lastFileCount = imagePathsTmp.Count();
+            imagePathsTmp = imagePathsTmp.Where(path => SupportFiles.Contains(path.ToUpper().Split('.')[^1]));
             switch (Sort)
             {
                 case ImagePickerSort.Name:
-                    _imagePaths = _imagePaths.OrderBy(path => path);
+                    imagePathsTmp = imagePathsTmp.OrderBy(path => path);
                     break;
                 case ImagePickerSort.Name_Reverse:
-                    _imagePaths = _imagePaths.OrderByDescending(path => path);
+                    imagePathsTmp = imagePathsTmp.OrderByDescending(path => path);
                     break;
                 case ImagePickerSort.CreationTime:
-                    _imagePaths = _imagePaths.OrderBy(path => File.GetCreationTime(path));
+                    imagePathsTmp = imagePathsTmp.OrderBy(path => File.GetCreationTime(path));
                     break;
                 case ImagePickerSort.CreationTime_Reverse:
-                    _imagePaths = _imagePaths.OrderByDescending(path => File.GetCreationTime(path));
+                    imagePathsTmp = imagePathsTmp.OrderByDescending(path => File.GetCreationTime(path));
                     break;
                 case ImagePickerSort.LastWriteTime:
-                    _imagePaths = _imagePaths.OrderBy(path => File.GetLastWriteTime(path));
+                    imagePathsTmp = imagePathsTmp.OrderBy(path => File.GetLastWriteTime(path));
                     break;
                 case ImagePickerSort.LastWriteTime_Reverse:
-                    _imagePaths = _imagePaths.OrderByDescending(path => File.GetLastWriteTime(path));
+                    imagePathsTmp = imagePathsTmp.OrderByDescending(path => File.GetLastWriteTime(path));
                     break;
                 case ImagePickerSort.Random:
                     _randomSeed = (int)DateTime.Now.Ticks;
                     Random random = new Random(_randomSeed);
-                    _imagePaths = _imagePaths.OrderBy(path => random.Next(2));
+                    imagePathsTmp = imagePathsTmp.OrderBy(path => random.Next(2));
                     break;
                 case ImagePickerSort.Random_NotRepeating:
                     Random random2 = new Random(_randomSeed);
-                    _imagePaths = _imagePaths.OrderBy(path => random2.Next(2));
+                    imagePathsTmp = imagePathsTmp.OrderBy(path => random2.Next(2));
                     break;
             }
+            _imagePaths = imagePathsTmp.ToArray();
         }
         private void _CheckAllFolder()
         {
